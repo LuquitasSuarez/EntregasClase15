a75cd4f32ff9dbe4314e157d93f4d99d8698d7d0
Lucas Suarez <suarez.lucas.f@gmail.com>
2022-01-04 22:50:35 -0300

    agregando js

---
+++ producto.js
@@ -0,0 +1,11 @@
+class Producto{
+    constructor(nombre, descripcion, precio, img) {
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.precio = precio
+        this.img = img
+        this.cant = 1
+    }
+}
+
+let productos = []
